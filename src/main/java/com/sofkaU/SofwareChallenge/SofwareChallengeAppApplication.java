bd8f1a452f3ef96780ceb3a8f94aeda34273196b
Angel Agelvis <agelvisangel@gmail.com>
2022-05-05 22:07:53 -0500

    project initialized

---
+++ src/main/java/com/sofkaU/SofwareChallenge/SofwareChallengeAppApplication.java
@@ -0,0 +1,13 @@
+package com.sofkaU.SofwareChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofwareChallengeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofwareChallengeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkaU/SofwareChallenge/SofwareChallengeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkaU.SofwareChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofwareChallengeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
